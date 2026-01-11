ef2a837360c6d77eb3ff634b7c1cf073868da9c1
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-01-10 23:03:40 -0700

    Passed off all Queen tests

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -1,11 +1,64 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class QueenMovesCalculator implements PieceMovesCalculator {
     @Override
-    public Collection<ChessMove> calcMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+    public Collection<ChessMove> calcMoves(ChessBoard board, ChessPosition startPosition) {
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        moves.addAll(calcMovesHelper(board, startPosition, 1, 1));
+        moves.addAll(calcMovesHelper(board, startPosition, 1, -1));
+        moves.addAll(calcMovesHelper(board, startPosition, -1, 1));
+        moves.addAll(calcMovesHelper(board, startPosition, -1, -1));
+        moves.addAll(calcMovesHelper(board, startPosition, 1, 0));
+        moves.addAll(calcMovesHelper(board, startPosition, 0, 1));
+        moves.addAll(calcMovesHelper(board, startPosition, -1, 0));
+        moves.addAll(calcMovesHelper(board, startPosition, 0, -1));
+
+        return moves;
+    }
+
+    private Collection<ChessMove> calcMovesHelper(
+            ChessBoard board,
+            ChessPosition startingPosition,
+            int rowDirection,
+            int colDirection) {
+
+        ChessPiece movingPiece = board.getPiece(startingPosition);
+        ChessGame.TeamColor teamColor = movingPiece.getTeamColor();
+        Collection<ChessMove> movesByDirection = new ArrayList<>();
+        int startRow = startingPosition.getRow();
+        int startCol = startingPosition.getColumn();
+        int currentRow = startRow;
+        int currentCol = startCol;
+
+        while (true) {
+            currentRow += rowDirection;
+            currentCol += colDirection;
+
+            if (!isOnBoard(currentRow, currentCol)) {
+                break;
+            }
+
+            ChessPosition currentPosition = new ChessPosition(currentRow, currentCol);
+            ChessPiece pieceAtPosition = board.getPiece((currentPosition));
+
+            if (pieceAtPosition == null) {
+                movesByDirection.add(new ChessMove(startingPosition, currentPosition, null));
+            } else if (pieceAtPosition.getTeamColor() == teamColor) {
+                break;
+            } else {
+                movesByDirection.add(new ChessMove(startingPosition, currentPosition, null));
+                break;
+            }
+        }
+        return movesByDirection;
+    }
+
+    private boolean isOnBoard(int row, int col) {
+        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
     }
 }
