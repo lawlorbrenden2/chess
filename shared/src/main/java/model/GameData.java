f9de5820645e7f34a40ac2dae10613cb5e8b65d6
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-02-15 18:03:15 -0700

    Updated record classes to be actual records and added DAO interfaces

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,12 @@
 package model;
 
-public class GameData {
+import chess.ChessGame;
+
+public record GameData(
+        Integer gameID,
+        String whiteUsername,
+        String blackUsername,
+        String gameName,
+        ChessGame game
+) {
 }
