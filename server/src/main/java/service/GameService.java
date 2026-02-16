2bbcf8953044eb1b2b2a4abb8ac3d72bdcd63b1e
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-02-15 18:28:25 -0700

    Added service classes for user, game, and clear endpoints

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class GameService {
+    public ListGamesResult listGames(ListGamesRequest listGamesRequest) {}
+    public CreateGameResult createGame(CreateGameRequest createGameRequest) {}
+    public JoinGameResult joinGame(JoinGameRequest joinGameRequest) {}
+}
