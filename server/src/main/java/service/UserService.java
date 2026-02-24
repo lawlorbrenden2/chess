55848e72a18f2b944824f36c83bae5ce813273ca
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-02-23 22:33:15 -0700

    Wrote register result in user service and added an already taken exception class

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,13 +1,48 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+
+import model.data.AuthData;
+import model.data.UserData;
 import model.request.LoginRequest;
 import model.request.LogoutRequest;
 import model.request.RegisterRequest;
 import model.result.LoginResult;
 import model.result.RegisterResult;
 
+import java.util.UUID;
+
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest) {}
+
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws AlreadyTakenException, DataAccessException {
+        // check if username already exists
+        if (userDAO.getUser(request.username()) != null) {
+            throw new AlreadyTakenException("Username already taken");
+        }
+
+        // create user data
+        UserData user = new UserData(request.username(), request.password(), request.email());
+        userDAO.createUser(user);
+
+        // generate auth token
+        String token = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(token, request.username());
+        authDAO.createAuth(auth);
+
+        // return register result
+        return new RegisterResult(request.username(), token);
+    }
+
     public LoginResult login(LoginRequest loginRequest) {}
     public void logout(LogoutRequest logoutRequest) {}
 }
