7c714b1176f272064a18ddda538e0d5dc5d9711b
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-02-26 20:43:58 -0700

    Begin making tests for register

---
+++ server/src/test/java/service/UserServiceTests.java
@@ -0,0 +1,19 @@
+package service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTests {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void register() {
+    }
+}
