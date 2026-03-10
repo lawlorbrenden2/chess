57441e16a5c8496c4aa19974a49c4d69ad10e45d
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-03-09 18:43:18 -0600

    finished writing database configurer

--- server/src/main/java/dataaccess/sqldao/DatabaseConfigurer.java
+++ server/src/main/java/dataaccess/sqldao/DatabaseConfigurer.java
@@ -1,4 +1,54 @@
 package dataaccess.sqldao;
 
+import dataaccess.DataAccessException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseConfigurer {
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                json TEXT DEFAULT NULL,
+                PRIMARY KEY (username)
+            )
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS auths (
+                authToken VARCHAR(255) NOT NULL,
+                username VARCHAR(255) NOT NULL,
+                json TEXT DEFAULT NULL,
+                PRIMARY KEY (authToken)
+            )
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                gameID INT NOT NULL AUTO_INCREMENT,
+                gameName VARCHAR(255),
+                whiteUsername VARCHAR(255),
+                blackUsername VARCHAR(255),
+                json TEXT,
+                PRIMARY KEY (gameID)
+            )
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
