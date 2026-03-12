a646d8e17ebfd9960fa32d5a30377cae48b17263
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-03-11 20:05:42 -0600

    Wrote SQLAuthDAO

--- server/src/main/java/dataaccess/sqldao/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/sqldao/SQLAuthDAO.java
@@ -1,9 +1,16 @@
 package dataaccess.sqldao;
 
+import com.google.gson.Gson;
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import model.data.AuthData;
+import model.data.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLAuthDAO extends BaseSQLDAO implements AuthDAO {
@@ -13,26 +20,58 @@ public class SQLAuthDAO extends BaseSQLDAO implements AuthDAO {
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
-
+        var statement = "INSERT INTO auths (authToken, username, json) VALUES (?, ?, ?)";
+        String json = new Gson().toJson(auth);
+        executeUpdate(statement, auth.authToken(), auth.username(), json);
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT json FROM auths WHERE authToken=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String json = rs.getString("json");
+                        return new Gson().fromJson(json, AuthData.class);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to read data: " + e.getMessage());
+        }
         return null;
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
-
+        var statement = "DELETE FROM auths WHERE authToken=?";
+        executeUpdate(statement, authToken);
     }
 
     @Override
     public List<AuthData> listAuths() throws DataAccessException {
-        return List.of();
+        var result = new ArrayList<AuthData>();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT json FROM auths";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        String json = rs.getString("json");
+                        result.add(new Gson().fromJson(json, AuthData.class));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to read data: " + e.getMessage());
+        }
+        return result;
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        var statement = "TRUNCATE auths";
+        executeUpdate(statement);
     }
 }
