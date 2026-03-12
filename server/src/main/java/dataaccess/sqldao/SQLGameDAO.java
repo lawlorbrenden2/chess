3bf514e7fb3bf8703f89e66d6d29c07d32aa736f
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-03-11 19:53:22 -0600

    Wrote listGames, getGame, and clear games

--- server/src/main/java/dataaccess/sqldao/SQLGameDAO.java
+++ server/src/main/java/dataaccess/sqldao/SQLGameDAO.java
@@ -4,7 +4,13 @@ import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import model.data.GameData;
+import model.data.UserData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLGameDAO extends BaseSQLDAO implements GameDAO {
@@ -23,12 +29,40 @@ public class SQLGameDAO extends BaseSQLDAO implements GameDAO {
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT json FROM games WHERE gameID=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String json = rs.getString("json");
+                        return new Gson().fromJson(json, GameData.class);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to read data: " + e.getMessage());
+        }
         return null;
     }
 
     @Override
     public List<GameData> listGames() throws DataAccessException {
-        return List.of();
+        var result = new ArrayList<GameData>();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT json FROM games";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        String json = rs.getString("json");
+                        result.add(new Gson().fromJson(json, GameData.class));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to read data: " + e.getMessage());
+        }
+        return result;
     }
 
     @Override
@@ -38,6 +72,7 @@ public class SQLGameDAO extends BaseSQLDAO implements GameDAO {
 
     @Override
     public void clear() throws DataAccessException {
-
+        var statement = "TRUNCATE games";
+        executeUpdate(statement);
     }
 }
