5665149f4296c944ad77cc9f678b7bb828e7abb8
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-03-18 20:35:15 -0600

    Continue working on server facade

--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -7,17 +7,21 @@ import model.result.RegisterResult;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
     private final String serverURL;
+    private final Gson gson = new Gson();
 
-    public ServerFacade(String serverPort) {
+    public ServerFacade(String serverURL) {
         this.serverURL = serverURL;
     }
 
     public RegisterResult register(UserData user) throws Exception {
-        var request = buildRequest("Post", "/user", user);
+        var request = buildRequest("POST", "/user", user);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterResult.class);
     }
 
     private HttpRequest buildRequest(String method, String path, Object body) {
@@ -32,9 +36,37 @@ public class ServerFacade {
 
     private HttpRequest.BodyPublisher makeRequestBody(Object request) {
         if (request != null) {
-            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+            return HttpRequest.BodyPublishers.ofString(gson.toJson(request));
         } else {
             return HttpRequest.BodyPublishers.noBody();
         }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws Exception {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new Exception("Server error: " + ex.getMessage(), ex);        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws Exception {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw new Exception("Server error: " + body);
+            }
+
+            throw new Exception("HTTP error: " + status);        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 }
