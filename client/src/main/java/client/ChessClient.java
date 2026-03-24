8a6e21a7b03ba74438a78d43e50c37ca0f88e3db
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-03-23 19:01:08 -0600

    Chess client changes

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -16,4 +16,52 @@ public class ChessClient {
     public ChessClient(String serverURL) {
         server = new ServerFacade(serverURL);
     }
+
+    public void run() {
+        System.out.println("👑 Welcome to 240 chess. Type Help to get started. 👑");
+        System.out.println(help());
+
+        Scanner scanner = new Scanner(System.in);
+        String result = "";
+
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.println(result);
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private void printPrompt() {
+
+    }
+
+    public String eval(String input) {
+        return "";
+    }
+
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return """
+                    register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                    login <USERNAME> <PASSWORD> - to play chess
+                    quit - playing chess
+                    help - with possible commands
+                    """;
+        }
+        return """
+                create <NAME> - a game
+                list - games
+                join <ID> [WHITE|BLACK] - a game
+                observe <ID> - a game
+                logout - when you are done
+                quit - playing chess
+                help - with possible commands
+                """;
+    }
 }
