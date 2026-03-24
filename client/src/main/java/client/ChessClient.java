800f6a7e49866828ae2d2e06c9989f6124af2d88
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-03-23 18:28:18 -0600

    Added state enum

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,4 +1,19 @@
 package client;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+import server.ServerFacade;
+import com.google.gson.Gson;
+import model.*;
+import static ui.EscapeSequences.*;
+
 public class ChessClient {
+    private String authToken = null;
+    private final ServerFacade server;
+    private State state = State.SIGNEDOUT;
+
+    public ChessClient(String serverURL) {
+        server = new ServerFacade(serverURL);
+    }
 }
