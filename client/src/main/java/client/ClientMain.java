e6c9d074cb105203416ad5a2fbdc42d7c99ef0e9
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-03-25 12:28:30 -0600

    minor change to comments

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,14 +1,12 @@
 package client;
 
-import chess.ChessGame;
-import chess.ChessPiece;
 
 public class ClientMain {
     public static void main(String[] args) {
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Client: " + piece);
 
-        String serverURL = "http://localhost:8080"; // adjust if needed
+        String serverURL = "http://localhost:8080";
 
         var client = new ChessClient(serverURL);
         client.run();
