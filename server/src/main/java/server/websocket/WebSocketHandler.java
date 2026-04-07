8ab8bfe495b84167df6706aee0f08269b7e1a2a2
Brenden Lawlor <lawlorbrenden2@gmail.com>
2026-04-06 20:55:05 -0600

    Implemented barebones websocket hander

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,35 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import websocket.commands.*;
+import com.google.gson.Gson;
+import io.javalin.Javalin;
+import websocket.messages.ErrorMessage;
+import websocket.messages.NotificationMessage;
+
 public class WebSocketHandler {
+
+    Gson gson = new Gson();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) {
+        try {
+            UserGameCommand command = gson.fromJson(message,UserGameCommand.class);
+                switch (command.getCommandType()) {
+                    case CONNECT -> {
+                        session.getRemote().sendString(
+                                gson.toJson(new NotificationMessage("Connected!"))
+                        );
+                    }
+                    default -> {
+                        session.getRemote().sendString(
+                                gson.toJson(new ErrorMessage("Unknown command"))
+                        );
+                    }
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
